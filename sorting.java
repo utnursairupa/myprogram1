0705f6c8e8c7e84187f1670cd5b501b60f68ce82
utnursairupa <utnursairupa@gmail.com>
2025-03-21 09:30:16 -0700

    Create sorting.java

---
+++ sorting.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SortArray {
+
+ public static void main(String[] args) {
+
+ int[] array = {5, 3, 9, 1, 7};
+
+ Arrays.sort(array);
+
+ System.out.println(“Sorted array in ascending order: ” + Arrays.toString(array));
+
+ }
+
+}
