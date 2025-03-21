643e1f42a65e4acf70b7ec2bdbd0fbb096c92678
utnursairupa <utnursairupa@gmail.com>
2025-03-21 09:24:30 -0700

    Create multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class MultiplyTwoNumbers {
+
+ public static void main(String[] args) {
+
+ double first = 2.5, second = 4.5;
+
+ double product = first * second;
+
+ System.out.println(“The product is: ” + product);
+
+ }
+
+}
