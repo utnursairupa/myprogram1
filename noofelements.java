ff343b0b876fb56114b5d48a74dc7ad41877370b
utnursairupa <utnursairupa@gmail.com>
2025-03-21 09:31:19 -0700

    Create noofelements.java

---
+++ noofelements.java
@@ -0,0 +1,13 @@
+public class ArrayLength {
+
+ public static void main(String[] args) {
+
+ int[] array = {1, 2, 3, 4, 5};
+
+ int length = array.length;
+
+ System.out.println(“Number of elements in the array: ” + length);
+
+ }
+
+}
