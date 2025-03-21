3ab164deba413aa83acadd6dd8cf7d7b6db95b0e
utnursairupa <utnursairupa@gmail.com>
2025-03-21 09:27:27 -0700

    Create compoundinterest.java

---
+++ compoundinterest.java
@@ -0,0 +1,13 @@
+public class CompoundInterest {
+
+ public static void main(String[] args) {
+
+ double principal = 15000, rate = 5.5, time = 3;
+
+ double compoundInterest = principal * (Math.pow((1 + rate / 100), time)) – principal;
+
+ System.out.println(“Compound Interest: ” + compoundInterest);
+
+ }
+
+}
